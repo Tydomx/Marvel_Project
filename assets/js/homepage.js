9209741859479e92dd59aedd0694e602a4dcd491
Tydomx <99767019+Tydomx@users.noreply.github.com>
2022-05-02 19:59:44 -0500

    Revert "Feature/navbar links"

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,22 +2,13 @@ document.addEventListener('DOMContentLoaded',function(){
   const form = document.getElementById('textarea1')
   const box = document.querySelector('.modal');
   M.Modal.init(box,{
-    dismissible:false,
     onOpenEnd: function(modal,trigger) {
       const charPage = document.getElementById('charPage')
       charPage.addEventListener('click', () => {
+        console.log('hello world')
         localStorage.setItem("charSelected", form.value);
       });
       
     }
   });
 });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.dropdown-trigger');
-     elems[0].setAttribute("style", "background:#E57373;")
-    var instances = M.Dropdown.init(elems, {
-      coverTrigger:false,
-      constrainWidth:false
-    });
-  });
